d5d61de97832d48ecbfb27c0d13489ab1dda7181
Aown Raza <Aown.Raza@didactic-innovations.de>
2024-07-06 14:05:43 +0200

    .

--- node_server.js
+++ node_server.js
@@ -1,35 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // Import CORS module
+const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors()); // Enable CORS for all routes
+app.use(cors());
 
-// Initialize face_found variable (simulating a condition)
-let face_found = true;
+let face_found = true; // Initialize face_found variable
 
-// GET endpoint to return different messages based on face_found value
 app.get('/api/face-status', (req, res) => {
-    if (face_found) {
-        res.json(face_found);
-    } else { 
-        face_found = true
-        res.json(false);
-    }
+    res.json(face_found); // Return the current state of face_found
 });
 
-// POST endpoint to update face_found based on Flask server's request
 app.post('/update-face-status', (req, res) => {  
-    console.log(`post req`)
+    console.log(`post req`);
     const { face_found: newFaceFound } = req.body;
     
-    face_found = newFaceFound;
+    face_found = newFaceFound; // Update face_found based on the request
     console.log(`Updated face_found to: ${face_found}`);
     res.json({ message: `Face status updated to ${face_found}` });
 });
 
-// Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
