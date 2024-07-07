65af47a4d38d62d41e6a23cab814b96da5aac993
Aown Raza <Aown.Raza@didactic-innovations.de>
2024-07-07 16:51:35 +0200

    .

--- server/rest_api.py
+++ server/rest_api.py
@@ -278,7 +278,7 @@ def detect_audio():
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
     RATE = 44100
-    THRESHOLD = 1000
+    THRESHOLD = 200
 
     audio = pyaudio.PyAudio()
     stream = audio.open(format=FORMAT, channels=CHANNELS,
